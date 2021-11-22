54e9203efca35a487e5afddb52ce0066601b322a
JVlatt <matthieu.manuellan@gmail.com>
2021-11-22 13:08:54 +0100

    phylo scale

--- Assets/Scripts/MusicAnalyzer/Phyllotaxis.cs
+++ Assets/Scripts/MusicAnalyzer/Phyllotaxis.cs
@@ -24,17 +24,25 @@ public class Phyllotaxis : MonoBehaviour
     private int _number;
     private int _currentIteration;
 
-    public bool _spawnObject;
-    public GameObject _objectToSpawn;
-    public Vector3 _objectScale;
     private Vector2 m_currentPos;
 
     private TrailRenderer _trailRenderer;
 
+    //Repeat
+    private bool _forward;
+    public bool _repeat, _invert;
+
+    //Scaling
+    public bool _useScaleAnimation, _useScaleCurve;
+    public Vector2 _scaleAnimMinMax;
+    public AnimationCurve _scaleAnimCurve;
+    public float _scaleAnimSpeed;
+    public int _scaleBand;
+    private float _scaleTimer, _currentScale;
 
     void SetLerpPosition()
     {
-        m_currentPos = CalculatePhyllo(_degree, _scale, _number);
+        m_currentPos = CalculatePhyllo(_degree, _currentScale, _number);
         _startPos = this.transform.localPosition;
         _endPos = m_currentPos;
     }
@@ -54,7 +62,8 @@ public class Phyllotaxis : MonoBehaviour
 
     void Awake()
     {
-        if (_spawnObject) return;
+        _currentScale = _scale;
+        _forward = true;
 
         _trailRenderer = GetComponent<TrailRenderer>();
         _trailMat = new Material(_trailRenderer.material);
@@ -62,7 +71,7 @@ public class Phyllotaxis : MonoBehaviour
         _trailRenderer.material = _trailMat;
 
         _number = _numberStart;
-        transform.localPosition = CalculatePhyllo(_degree, _scale, _number);
+        transform.localPosition = CalculatePhyllo(_degree, _currentScale, _number);
         if (_useLerping)
         {
             _isLerping = true;
@@ -70,48 +79,28 @@ public class Phyllotaxis : MonoBehaviour
         }
     }
 
-    //void FixedUpdate()
-    //{
-    //    if (_spawnObject) return;
-        
-    //    if (_useLerping)
-    //    {
-    //        if (_isLerping)
-    //        {
-    //            float timeSinceStarted = Time.time - _timeStartedLerp;
-    //            float percentageComplete = timeSinceStarted / _intervalLerp;
-    //            transform.localPosition = Vector3.Lerp(_startPos, _endPos, percentageComplete);
-
-    //            if (percentageComplete >= 0.97f)
-    //            {
-    //                transform.localPosition = _endPos;
-    //                _number += _stepSize;
-    //                _currentIteration++;
-    //                if (_currentIteration <= _maxIteration)
-    //                {
-    //                    StartLerping();
-    //                }
-    //                else
-    //                {
-    //                    _isLerping = false;
-    //                }
-    //            }
-    //        }
-    //    }
-    //    else
-    //    {
-    //        m_currentPos = CalculatePhyllo(_degree, _scale, _number);
-    //        transform.localPosition = m_currentPos;
-    //        _number+= _stepSize;
-    //        _currentIteration++;
-    //    }
-        
-    //}
-
-
     // Update is called once per frame
     void Update()
     {
+        if (_useScaleAnimation)
+        {
+            if (_useScaleCurve)
+            {
+                _scaleTimer += Time.deltaTime * _scaleAnimSpeed * AudioPeer._audioBand[_scaleBand];
+                if (_scaleTimer >= 1)
+                {
+                    _scaleTimer -= 1;
+                }
+
+                _currentScale = Mathf.Lerp(_scaleAnimMinMax.x, _scaleAnimMinMax.y,
+                    _scaleAnimCurve.Evaluate(_scaleTimer));
+            }
+            else
+            {
+                _currentScale = Mathf.Lerp(_scaleAnimMinMax.x, _scaleAnimMinMax.y, AudioPeer._audioBand[_scaleBand]);
+            }
+        }
+
         if (_useLerping)
         {
             if (_isLerping)
@@ -128,27 +117,53 @@ public class Phyllotaxis : MonoBehaviour
                 if (_lerpPosTimer >= 1)
                 {
                     _lerpPosTimer -= 1;
-                    _number += _stepSize;
-                    _currentIteration++;
-                    SetLerpPosition();
+
+                    if (_forward)
+                    {
+                        _number += _stepSize;
+                        _currentIteration++;
+
+                    }
+                    else
+                    {
+                        _number -= _stepSize;
+                        _currentIteration--;
+                    }
+
+                    if (_currentIteration > 0 && _currentIteration < _maxIteration)
+                    {
+                        SetLerpPosition();
+                    }
+                    else
+                    {
+                        if (_repeat)
+                        {
+                            if (_invert)
+                            {
+                                _forward = !_forward;
+                                SetLerpPosition();
+                            }
+                            else
+                            {
+                                _number = _numberStart;
+                                _currentIteration = 0;
+                                SetLerpPosition();
+                            }
+                        }
+                        else
+                        {
+                            _isLerping = false;
+                        }
+                    }
                 }
             }
         }
         if(!_useLerping)
         {
-            m_currentPos = CalculatePhyllo(_degree, _scale, _number);
+            m_currentPos = CalculatePhyllo(_degree, _currentScale, _number);
             transform.localPosition = m_currentPos;
             _number += _stepSize;
             _currentIteration++;
-        }
-        if (!_spawnObject) return;
-        if (Input.GetKey(KeyCode.Space))
-        {
-            m_currentPos = CalculatePhyllo(_degree, _scale, _number);
-            GameObject objectInstance = Instantiate(_objectToSpawn);
-            objectInstance.transform.position = m_currentPos;
-            objectInstance.transform.localScale = _objectScale;
-            _number++;
         }
     }
 }
